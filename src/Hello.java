324ec9762463737861c262442fa64e1bb7819399
Eric <schaikejgv@gmail.com>
2017-06-26 14:40:02 +0200

    hello

--- src/Hello.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Eric on 26-Jun-17.
- */
-public class Hello {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
--- src/MyLanguage.g4
+++ src/MyLanguage.g4
@@ -19,6 +19,3 @@ RIGHT : ')';
 
 // ignore whitespace
 WS : [ \t\n\r] -> skip;
-
-// everything else is a typo
-TYPO : [a-zA-Z]+;
